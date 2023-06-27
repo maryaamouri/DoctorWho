3833d8195fe79529c29f3045e64a8947927b18fe
Marya_Amouri <marya.ammori@gmail.com>
2023-06-27 05:06:42 +0300

    create Irepo interface

---
+++ DoctorWhoDomain/Repos/IEpisodeRepo.cs
@@ -0,0 +1,8 @@
+﻿namespace DoctorWhoDomain.Repos
+{
+    public interface IEpisodeRepo<T> : IRepo<T> where T : class
+    {
+        public void AddCompanionToEpisode(T Entity);
+        public void AddEnemyToEpisode(T Entity);
+    }
+}
